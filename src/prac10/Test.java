1a8b6deca75189cc8422f98f5e3d08678834a07c
Vladislav-Erofeev <92443812+Vladislav-Erofeev@users.noreply.github.com>
2022-10-13 11:48:29 +0300

    Practice 10

--- src/prac10/Test.java
+++ src/prac10/Test.java
@@ -8,17 +8,25 @@ public class Test
     {
         ArrayList<Student> arr = new ArrayList<Student>(0);
         arr.add(new Student("Alexey", "Ivanov", 1, 2, 2.6));
-        arr.add(new Student("Alexey", "Ivanov", 1, 2, 2.5));
+        arr.add(new Student("Alexey", "Ivanov", 1, 4, 2.5));
         arr.add(new Student("Alexey", "Ivanov", 1, 2, 4.2));
-        arr.add(new Student("Alexey", "Ivanov", 1, 2, 5));
-        arr.add(new Student("Alexey", "Ivanov", 1, 2, 3.2));
-        arr.add(new Student("Alexey", "Ivanov", 1, 2, 2.2));
-        ScoreComparator comparator = new ScoreComparator();
+        arr.add(new Student("Alexey", "Ivanov", 1, 1, 5));
+        arr.add(new Student("Alexey", "Ivanov", 1, 6, 3.2));
+        arr.add(new Student("Alexey", "Ivanov", 1, 3, 2.2));
         SortingStudents obj = new SortingStudents(arr);
+        // Устанавливаем компаратор на сравнение балла
+        ScoreComparator comparator = new ScoreComparator();
         obj.setComparator(comparator);
         obj.outArray();
         obj.sort();
         System.out.println();
+        obj.outArray();
+        System.out.println();
+
+        // устанавливаем компаратор на сравнение номера группы
+        GroupComparator groupComparator = new GroupComparator();
+        obj.setComparator(groupComparator);
+        obj.sort();
         obj.outArray();
     }
 }
