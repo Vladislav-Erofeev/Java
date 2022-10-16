0e6b04f58003c29877160cc945d00bb01e4dd277
Vladislav <vlad.erofeev1911@gmail.com>
2022-10-16 11:18:54 +0300

    Test

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        ArrayList<Integer> arr;
+        System.out.println("Hi");
     }
 }
